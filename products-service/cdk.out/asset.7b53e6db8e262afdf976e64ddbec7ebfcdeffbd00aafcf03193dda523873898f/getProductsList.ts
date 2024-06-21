8945c7defb7e2522f552c205520594b94b3f8d27
Ivan Lashuk <bbritva1910@gmail.com>
2024-06-21 16:53:26 +0400

    feat: change lambda functions to work with DB

--- products-service/cdk.out/asset.7b53e6db8e262afdf976e64ddbec7ebfcdeffbd00aafcf03193dda523873898f/getProductsList.ts
+++ products-service/cdk.out/asset.7b53e6db8e262afdf976e64ddbec7ebfcdeffbd00aafcf03193dda523873898f/getProductsList.ts
@@ -1,21 +1,41 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
-import { products as Products } from "./products";
-import { IProducts } from "../models/products";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+const getAllProducts = async () => {
+  const command = new ScanCommand({
+    TableName: "Product",
+  });
+
+  const response = await docClient.send(command);
+  return response.Items;
+};
+
+const getAllStockData = async () => {
+  const command = new ScanCommand({
+    TableName: "Stock",
+  });
+
+  const response = await docClient.send(command);
+  return response.Items;
+};
 
 exports.handler = async function (event: APIGatewayProxyEvent) {
-  const products: IProducts[] = Products;
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  try {
+    const products = await getAllProducts();
+    const stockData = await getAllStockData();
 
-  if (products.length === 0) {
-    return {
-      statusCode: 404,
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({ message: "Products not found" }),
-    };
-  } else {
+    const combinedData = products?.map((i) => {
+      const stockProduct = stockData?.find((e) => e.product_id === i.id) || {
+        count: 0,
+      };
+      return { ...i, count: stockProduct.count };
+    });
+
     return {
       statusCode: 200,
       headers: {
@@ -23,7 +43,17 @@ exports.handler = async function (event: APIGatewayProxyEvent) {
         "Cache-Control": "no-cache",
         "Access-Control-Allow-Origin": "*",
       },
-      body: JSON.stringify(products),
+      body: JSON.stringify(combinedData),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ error }),
     };
   }
 };
