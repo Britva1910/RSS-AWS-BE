9efebcc8a07f95a24764bbbb0ebfb9551cfbf9a1
Ivan Lashuk <bbritva1910@gmail.com>
2024-06-29 15:03:56 +0400

    ref: refactor tests

--- import-service/test/importFileParser.test.ts
+++ import-service/test/importFileParser.test.ts
@@ -1,136 +1,104 @@
-import {S3Client, GetObjectCommand, CopyObjectCommand, DeleteObjectCommand, PutObjectCommand, PutObjectCommandOutput} from '@aws-sdk/client-s3';
-import {mockClient} from 'aws-sdk-client-mock';
-import {Readable} from 'stream';
-import {sdkStreamMixin} from '@smithy/util-stream';
-import 'aws-sdk-client-mock-jest';
-const {handler} = require('../lambda-functions/importFileParser.js');
+import {
+  S3Client,
+  GetObjectCommand,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import { mockClient } from "aws-sdk-client-mock";
+import { Readable } from "stream";
+import { sdkStreamMixin } from "@smithy/util-stream";
+import "aws-sdk-client-mock-jest";
+const { handler } = require("../lambda-functions/importFileParser.js");
 
 const s3Mock = mockClient(S3Client);
+const event = {
+  Records: [
+    {
+      s3: {
+        object: {
+          key: "testKey",
+        },
+      },
+    },
+  ],
+};
 
-describe('importFileParser Lambda', () => {
-    beforeEach(() => {
-        s3Mock.reset();
+describe("importFileParser Lambda", () => {
+  beforeEach(() => {
+    s3Mock.reset();
+  });
+
+  it("should return a signed URL when a valid file name is provided", async () => {
+    s3Mock.on(GetObjectCommand).resolves({});
+
+    const consoleLogSpy = jest
+      .spyOn(console, "log")
+      .mockImplementation(() => {});
+
+    await handler(event);
+
+    expect(consoleLogSpy).toHaveBeenCalledWith(
+      "Error processing S3 event:",
+      expect.any(Error)
+    );
+    consoleLogSpy.mockRestore();
+  });
+  it("should log the result stream", async () => {
+    const stream = new Readable();
+    stream.push("header1,header2\nvalue1,value2\n");
+    stream.push(null);
+
+    const sdkStream = sdkStreamMixin(stream);
+
+    s3Mock.on(GetObjectCommand).resolves({
+      Body: sdkStream,
     });
 
-    it('should return a signed URL when a valid file name is provided', async () => {
-        s3Mock.on(GetObjectCommand).resolves({});
+    const consoleLogSpy = jest
+      .spyOn(console, "log")
+      .mockImplementation(() => {});
 
-        const event = {
-            Records: [
-                {
-                    s3: {
-                        object: {
-                            key: 'testKey'
-                        }
-                    }
-                }
-            ]
-        };
+    await handler(event);
 
-        const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+    expect(consoleLogSpy).toHaveBeenCalledWith("Result stream: ", [
+      { header1: "value1", header2: "value2" },
+    ]);
 
-        await handler(event);
+    consoleLogSpy.mockRestore();
+  });
+  it("should copy the object after processing the stream", async () => {
+    const stream = new Readable();
+    stream.push("header1,header2\nvalue1,value2\n");
+    stream.push(null);
 
-        expect(consoleLogSpy).toHaveBeenCalledWith('Error processing S3 event:', expect.any(Error));
-        consoleLogSpy.mockRestore();
-    });
-    it('should log the result stream', async () => {
-        const stream = new Readable();
-        stream.push('header1,header2\nvalue1,value2\n');
-        stream.push(null); // Завершаем поток
+    const sdkStream = sdkStreamMixin(stream);
 
-        const sdkStream = sdkStreamMixin(stream);
-
-        s3Mock.on(GetObjectCommand).resolves({
-            Body: sdkStream
-        });
-
-        const event = {
-            Records: [
-                {
-                    s3: {
-                        object: {
-                            key: 'testKey'
-                        }
-                    }
-                }
-            ]
-        };
-
-        const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
-
-        await handler(event);
-
-        expect(consoleLogSpy).toHaveBeenCalledWith('Result stream: ', [
-            { header1: 'value1', header2: 'value2' }
-        ]);
-
-        consoleLogSpy.mockRestore();
-    });
-    it('should copy the object after processing the stream', async () => {
-        const stream = new Readable();
-        stream.push('header1,header2\nvalue1,value2\n');
-        stream.push(null); // Завершаем поток
-
-        const sdkStream = sdkStreamMixin(stream);
-
-        s3Mock.on(GetObjectCommand).resolves({
-            Body: sdkStream
-        });
-
-        s3Mock.on(CopyObjectCommand).resolves({});
-
-        const event = {
-            Records: [
-                {
-                    s3: {
-                        object: {
-                            key: 'uploaded/testFile.csv'
-                        }
-                    }
-                }
-            ]
-        };
-
-        await handler(event);
-
-        const destinationKey = 'uploaded/testFile.csv'.replace('uploaded', 'parsed');
-
-        // Проверяем вызовы команды CopyObjectCommand
-        expect(s3Mock).toHaveReceivedCommand(CopyObjectCommand);
+    s3Mock.on(GetObjectCommand).resolves({
+      Body: sdkStream,
     });
 
-    it('should delete the object after processing the stream', async () => {
-        const stream = new Readable();
-        stream.push('header1,header2\nvalue1,value2\n');
-        stream.push(null); // Завершаем поток
+    s3Mock.on(CopyObjectCommand).resolves({});
 
-        const sdkStream = sdkStreamMixin(stream);
+    await handler(event);
 
-        s3Mock.on(GetObjectCommand).resolves({
-            Body: sdkStream
-        });
+    expect(s3Mock).toHaveReceivedCommand(CopyObjectCommand);
+  });
 
-        s3Mock.on(DeleteObjectCommand).resolves({});
+  it("should delete the object after processing the stream", async () => {
+    const stream = new Readable();
+    stream.push("header1,header2\nvalue1,value2\n");
+    stream.push(null);
 
-        const event = {
-            Records: [
-                {
-                    s3: {
-                        object: {
-                            key: 'uploaded/testFile.csv'
-                        }
-                    }
-                }
-            ]
-        };
+    const sdkStream = sdkStreamMixin(stream);
 
-        await handler(event);
-
-        const destinationKey = 'uploaded/testFile.csv'.replace('uploaded', 'parsed');
-
-        // Проверяем вызовы команды CopyObjectCommand
-        expect(s3Mock).toHaveReceivedCommand(DeleteObjectCommand);
+    s3Mock.on(GetObjectCommand).resolves({
+      Body: sdkStream,
     });
-})
-;
+
+    s3Mock.on(DeleteObjectCommand).resolves({});
+
+    await handler(event);
+
+    expect(s3Mock).toHaveReceivedCommand(DeleteObjectCommand);
+  });
+});
--- import-service/test/importProductFile.test.ts
+++ import-service/test/importProductFile.test.ts
@@ -1,57 +1,65 @@
-import {S3Client, PutObjectCommand, GetObjectCommand} from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { mockClient } from 'aws-sdk-client-mock';
-const { handler } = require('../lambda-functions/importProductsFile');
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { mockClient } from "aws-sdk-client-mock";
+const { handler } = require("../lambda-functions/importProductsFile");
 
-jest.mock('@aws-sdk/s3-request-presigner', () => ({
-    getSignedUrl: jest.fn(),
+jest.mock("@aws-sdk/s3-request-presigner", () => ({
+  getSignedUrl: jest.fn(),
 }));
 
 const s3Mock = mockClient(S3Client);
 
-describe('importProductsFile Lambda', () => {
-    beforeEach(() => {
-        s3Mock.reset();
-    });
+describe("importProductsFile Lambda", () => {
+  beforeEach(() => {
+    s3Mock.reset();
+  });
 
-    it('should return a signed URL when a valid file name is provided', async () => {
-        const event = {
-            queryStringParameters: {
-                name: 'testFile.csv'
-            }
-        };
+  it("should return a signed URL when a valid file name is provided", async () => {
+    const event = {
+      queryStringParameters: {
+        name: "testFile.csv",
+      },
+    } as any;
 
-        s3Mock.on(GetObjectCommand).resolves({});
+    s3Mock.on(PutObjectCommand).resolves({});
+    (getSignedUrl as jest.Mock) = jest
+      .fn()
+      .mockResolvedValue("https://signed-url.com");
+    const response = await handler(event);
 
-        const response = await handler(event);
+    expect(response.statusCode).toBe(200);
+    expect(JSON.parse(response.body).url).toBe("https://signed-url.com");
+  });
 
-        expect(response.statusCode).toBe(200);
-        expect(JSON.parse(response.body).url).toBe('https://signed-url.com');
-    });
+  it("should return a 400 error when no file name is provided", async () => {
+    const event = {
+      queryStringParameters: {},
+    };
 
-    it('should return a 400 error when no file name is provided', async () => {
-        const event = {
-            queryStringParameters: {}
-        };
+    const response = await handler(event);
 
-        const response = await handler(event);
+    expect(response.statusCode).toBe(400);
+    expect(JSON.parse(response.body).message).toBe(
+      "Missing file name in query parameters"
+    );
+  });
 
-        expect(response.statusCode).toBe(400);
-        expect(JSON.parse(response.body).message).toBe('Missing file name in query parameters');
-    });
+  it("should return a 500 error if getSignedUrl throws an error", async () => {
+    const event = {
+      queryStringParameters: {
+        name: "testFile.csv",
+      },
+    };
 
-    it('should return a 500 error if getSignedUrl throws an error', async () => {
-        const event = {
-            queryStringParameters: {
-                name: 'testFile.csv'
-            }
-        };
+    (getSignedUrl as jest.Mock) = jest
+      .fn()
+      .mockRejectedValue(new Error("Some error"));
 
-        (getSignedUrl as jest.Mock) = jest.fn().mockRejectedValue(new Error('Some error'));
+    const response = await handler(event);
 
-        const response = await handler(event);
-
-        expect(response.statusCode).toBe(500);
-        expect(JSON.parse(response.body).message).toBe('Could not generate signed URL');
-    });
+    expect(response.statusCode).toBe(500);
+    expect(JSON.parse(response.body).message).toBe(
+      "Could not generate signed URL"
+    );
+  });
 });
