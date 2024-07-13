5c5a0fcf112a18d8d411508b0a648c321f3e3d37
Ivan Lashuk <bbritva1910@gmail.com>
2024-07-13 18:18:32 +0400

    feat: implement auth stack and simple hendler

---
+++ authorization-service/lib/authorization-service-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import * as dotenv from "dotenv";
+
+export class AuthorizationServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const basicAuthorizer = new lambda.Function(this, "BasicAuthorizer", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("lambda-functions"),
+      handler: "basicAuthorizer.handler",
+    });
+
+    basicAuthorizer.grantInvoke(
+      new ServicePrincipal("apigateway.amazonaws.com")
+    );
+  }
+}
